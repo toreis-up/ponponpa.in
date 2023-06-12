154c4cebe6425d6d79cbad250512fc280fb7c8ba
toreis-up <toreis@reisan.dev>
2023-06-13 00:24:09 +0900

    feat: set ogp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: "icon", type: "image/x-icon", href: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f616.png" }
+      ],
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'ｵﾅｶｲﾀｰｲ' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'pͪoͣnͬpͣoͥnͭpͣa͡inͥ' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'ponponpa.in' },
+        { hid: 'og:title', property: 'og:title', content: 'pͪoͣnͬpͣoͥnͭpͣa͡inͥ' },
+        { hid: 'og:description', property: 'og:description', content: 'お腹が痛い時に共有しましょう。' },
+        { hid: 'og:image', property: 'og:image', content: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/1f616.png' },
+        { name: 'twitter:card', content: 'summary' }//　twitterの画像サイズ
       ]
     },
   },
